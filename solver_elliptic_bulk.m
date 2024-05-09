07ddab79733d156fe93788a2adaaa9c8e7d06d53
UBC Student <bzhang20@students.cs.ubc.ca>
2024-05-10 03:21:49 +0800

    added checkSPD function
    1. added checkSPD function
    2. modified solver_elliptic_bulk.m to have both "\" and pcg

--- solver_elliptic_bulk.m
+++ solver_elliptic_bulk.m
@@ -14,7 +14,28 @@ function u = solver_elliptic_bulk(D,alpha,f,P,M,K,R,bcond)
     LHS = D*Kdir + alpha*Mdir;
     rhs = M*f(P);
     RHS = rhs(bulknodes);
+
+    % start
+    tic 
     ubulk = LHS\RHS;
+    toc
+    
+
+    tic
+    % if (~checkSPD(LHS, 1e-12))
+    %     error("cannot use PCG")
+    % end
+    % TODO: prove mathematically that LHS is SPD
+    if (~isequal(LHS, LHS'))
+        % LHS is almost (but numerically not) symmetric positive definite
+        LHS = 0.5 * (LHS + LHS');
+    end
+    maxit = 500;
+    tol = 1e-10;
+    pre = ichol(LHS);
+    utest = pcg(LHS, RHS, tol, maxit, pre, pre');
+    toc
+    % end
     
     u = zeros(length(M),1);
     u(bulknodes,:) = ubulk;
